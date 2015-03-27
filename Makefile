736b62645c3550b03b3f2cdc9126fe266c1b96ab
Noah D. Brenowitz <nbren12@gmail.com>
2015-03-27 00:26:40 -0400

    bins are gathered, sorted, broadcasted

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 FIXME_SRCS = mpi_bug1.c mpi_bug2.c mpi_bug3.c mpi_bug4.c mpi_bug5.c mpi_bug6.c mpi_bug7.c
 FIXME_EXEC = $(FIXME_SRCS:.c=)
 
+CC = mpicc
+CFLAGS = -g
 
-all : $(FIXME_EXEC)
+all: $(FIXME_EXEC) ssort
 
-% : %.c
-	mpicc -o $@ $^
+
+clean: 
+	rm -f $(FIXME_EXEC) ssort
--- ssort.c
+++ ssort.c
@@ -21,18 +21,38 @@ static int compare(const void *a, const void *b)
 
 int main( int argc, char *argv[])
 {
-  int rank;
-  int i, N;
-  int *vec;
+  int rank, world_size;
+  int i, j, N, S;
+  int *vec, *samples, *sample_idxs;
+  int *all_samples;
+  int *bins, nsamp;
+  int randint;
+
+
+  int root = 0;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
   /* Number of random numbers per processor (this should be increased
    * for actual tests or could be passed in through the command line */
   N = 100;
 
+  /* Number of entries to send to root process. */
+  S = 10;
+  nsamp = S * world_size;
+
+
+  /* Allocations for all processes */
   vec = calloc(N, sizeof(int));
+  samples = calloc(N, sizeof(int));
+  sample_idxs = calloc(N, sizeof(int));
+  bins = calloc(world_size, sizeof(int));
+
+  /* Only need to allocate on root process */
+  if (rank == root) all_samples = calloc(nsamp, sizeof(int));
+
   /* seed random number generator differently on every core */
   srand((unsigned int) (rank + 393919));
 
@@ -42,19 +62,47 @@ int main( int argc, char *argv[])
   }
   printf("rank: %d, first entry: %d\n", rank, vec[0]);
 
-  /* sort locally */
-  qsort(vec, N, sizeof(int), compare);
 
   /* randomly sample s entries from vector or select local splitters,
    * i.e., every N/P-th entry of the sorted vector */
 
+  j = 0; 
+  do {
+    randint = rand() % N;
+
+    /* This logic ensures that the random draws are unique */
+    char isuniq = 1;
+    for (i = 0; i < j; i++) {
+      if ( sample_idxs[i] == randint) {
+	isuniq = 0;
+	break;
+      }
+    }
+
+    if (isuniq) {
+      sample_idxs[j] = randint;
+      samples[j] = vec[randint];
+      ++j;
+    }
+  } while(j < S);
+  
+
+  /* for (i = 0; i < nbins; ++i) printf("%d\n", bins[i]); */
   /* every processor communicates the selected entries
    * to the root processor; use for instance an MPI_Gather */
+  MPI_Gather(samples, S, MPI_INT, all_samples, S, MPI_INT, root, MPI_COMM_WORLD);
 
   /* root processor does a sort, determinates splitters that
    * split the data into P buckets of approximately the same size */
+  if ( rank == root ){
+    qsort(all_samples, nsamp, sizeof(int), compare);
+    for (i=0; i < world_size ; ++i) bins[i] = all_samples[(i+1) * S];
+    free(all_samples);
+  }
 
   /* root process broadcasts splitters */
+  MPI_Bcast(bins, world_size, MPI_INT, root, MPI_COMM_WORLD);
+
 
   /* every processor uses the obtained splitters to decide
    * which integers need to be sent to which other processor (local bins) */
@@ -64,11 +112,16 @@ int main( int argc, char *argv[])
    * with every processor how many integers it should expect,
    * and then use MPI_Send and MPI_Recv to exchange the data */
 
-  /* do a local sort */
+  /* sort locally */
+  qsort(vec, N, sizeof(int), compare);
 
   /* every processor writes its result to a file */
-
   free(vec);
+  free(samples);
+  free(sample_idxs);
+  free(bins);
+
   MPI_Finalize();
   return 0;
 }
+
